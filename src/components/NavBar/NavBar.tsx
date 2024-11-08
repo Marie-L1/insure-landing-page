5a4e936b7f7f0d5fee7bad6163f471d5f033e424
Maria Lukowich <mlukowich27@gmail.com>
2024-11-08 12:22:47 -0700

    setting up hamburger navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,32 @@
 import "./NavBar.scss"
+import { useState } from "react";
+import logo from "../../assets/images/logo.svg"
+import hambuger from "../../assets/images/icon-hamburger.svg"
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+  
   return (
-    <div>NarBar</div>
+    <nav className="navbar">
+      <img className="navbar__logo" src={logo} />
+
+      <button className="navbar__toggle" onClick={toggleMenu}>
+        <img src={hambuger} />
+      </button>
+
+      <ul className={`navbar__links ${menuOpen ? 'navbar__links--open' : ''}`}>
+        <li className="navbar__link"><a href="#home">Home</a></li>
+        <li className="navbar__link"><a href="#about">About</a></li>
+        <li className="navbar__link"><a href="#services">Services</a></li>
+        <li className="navbar__link"><a href="#contact">Contact</a></li>
+      </ul>
+
+
+    </nav>
   )
 }
 
